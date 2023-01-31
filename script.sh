7098ffd28e4c22043692527d41fa5cf96da69775
Mohit Gajjar <mohitgajjar501@outlook.com>
2023-01-31 10:56:15 +0530

    First program in shell script

--- script.sh
+++ script.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-IP_ADDRESS = ${ip addr show wlp2s0 | grep "inet "| cut -d" "}
+# IP_ADDRESS = ${ip addr show wlp2s0 | grep "inet "| cut -d" "}
+echo "Please enter the valid address: "
+read -p "IP ADDRESS: " IP1
+echo "You have entered $IP1"
